40c46e2551566dcf0e6a97b5697c23df799cc992
IronMan2297 <liamweigel1@icloud.com>
2024-08-03 22:34:46 +0200

    Items

---
+++ src/main/java/de/ironman/mccourse/item/ModCreativeModeTabs.java
@@ -0,0 +1,44 @@
+package de.ironman.mccourse.item;
+
+import de.ironman.mccourse.MCCourseMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MCCourseMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> BLACK_OPAL_ITEMS_TAB =
+            CREATIVE_MODE_TABS.register("black_opal_items_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.mccourse.black_opal_items_tab"))
+                    .icon(() -> new ItemStack(ModItems.BLACK_OPAL.get()))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.BLACK_OPAL.get());
+                        pOutput.accept(ModItems.RAW_BLACK_OPAL.get());
+
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> BLACK_OPAL_BLOCKS_TAB =
+            CREATIVE_MODE_TABS.register("black_opal_blocks_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.mccourse.black_opal_blocks_tab"))
+                    .icon(() -> new ItemStack(ModItems.RAW_BLACK_OPAL.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID, "black_opal_items_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(Blocks.ANDESITE);
+                        pOutput.accept(Blocks.DEEPSLATE_DIAMOND_ORE);
+
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+}
