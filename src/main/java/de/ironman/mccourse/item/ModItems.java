efb7a2d80054ba1ff852684602143dffd679f8c0
IronMan2297 <liamweigel1@icloud.com>
2024-08-04 16:33:07 +0200

    Tooltips

--- src/main/java/de/ironman/mccourse/item/ModItems.java
+++ src/main/java/de/ironman/mccourse/item/ModItems.java
@@ -3,10 +3,15 @@ package de.ironman.mccourse.item;
 import de.ironman.mccourse.MCCourseMod;
 import de.ironman.mccourse.item.custom.ChainsawItem;
 import de.ironman.mccourse.item.custom.FuelItem;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MCCourseMod.MOD_ID);
@@ -19,7 +24,13 @@ public class ModItems {
             ITEMS.registerItem("chainsaw", ChainsawItem::new, new Item.Properties().durability(32));
 
     public static final DeferredItem<Item> TOMATO =
-            ITEMS.registerItem("tomato", Item::new, new Item.Properties().food(ModFoodProperties.TOMATO));
+            ITEMS.registerItem("tomato", properties -> new Item(properties) {
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.mccourse.tomato.tooltip.1"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            }, new Item.Properties().food(ModFoodProperties.TOMATO));
     public static final DeferredItem<Item> FROSTFIRE_ICE =
             ITEMS.registerItem("frostfire_ice", properties -> new FuelItem(properties, 800), new Item.Properties());
 
