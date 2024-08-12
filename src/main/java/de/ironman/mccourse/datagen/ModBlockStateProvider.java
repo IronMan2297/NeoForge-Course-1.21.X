3280a4b0d35656299804189d23a25f507fe50003
IronMan2297 <liamweigel1@icloud.com>
2024-08-12 22:55:13 +0200

    doors and trapdoors

--- src/main/java/de/ironman/mccourse/datagen/ModBlockStateProvider.java
+++ src/main/java/de/ironman/mccourse/datagen/ModBlockStateProvider.java
@@ -20,7 +20,7 @@ public class ModBlockStateProvider extends BlockStateProvider {
         blockWithItem(ModBlocks.RAW_BLACK_OPAL_BLOCK);
 
         blockWithItem(ModBlocks.BLACK_OPAL_ORE);
-        blockWithItem(ModBlocks.DEEPSLATE_BLACK_OPAL_ORE );
+        blockWithItem(ModBlocks.DEEPSLATE_BLACK_OPAL_ORE);
         blockWithItem(ModBlocks.END_BLACK_OPAL_ORE);
         blockWithItem(ModBlocks.NETHER_BLACK_OPAL_ORE);
 
@@ -36,10 +36,15 @@ public class ModBlockStateProvider extends BlockStateProvider {
         fenceGateBlock(((FenceGateBlock) ModBlocks.BLACK_OPAL_FENCE_GATE.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
         wallBlock(((WallBlock) ModBlocks.BLACK_OPAL_WALL.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
 
+        doorBlockWithRenderType(((DoorBlock) ModBlocks.BLACK_OPAL_DOOR.get()), modLoc("block/black_opal_door_bottom"), modLoc("block/black_opal_door_top"), "cutout");
+        trapdoorBlockWithRenderType(((TrapDoorBlock) ModBlocks.BLACK_OPAL_TRAPDOOR.get()), modLoc("block/black_opal_trapdoor"), true, "cutout");
+
         blockItem(ModBlocks.BLACK_OPAL_STAIRS);
         blockItem(ModBlocks.BLACK_OPAL_SLAB);
         blockItem(ModBlocks.BLACK_OPAL_PRESSURE_PLATE);
         blockItem(ModBlocks.BLACK_OPAL_FENCE_GATE);
+
+        blockItem(ModBlocks.BLACK_OPAL_TRAPDOOR, "_bottom");
     }
 
     private void blockWithItem(DeferredBlock<Block> deferredBlock) {
@@ -48,5 +53,9 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     private void blockItem(DeferredBlock<Block> deferredBlock) {
         simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("mccourse:block/" + deferredBlock.getId().getPath()));
+    }
+
+    private void blockItem(DeferredBlock<Block> deferredBlock, String appendix) {
+        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("mccourse:block/" + deferredBlock.getId().getPath() + appendix));
     }
 }
