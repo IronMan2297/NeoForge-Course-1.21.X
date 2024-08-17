59b2b2b5b28cb7269af4269919034d4d18945e17
IronMan2297 <liamweigel1@icloud.com>
2024-08-17 18:43:03 +0200

    Custom Mob Variants

--- src/main/java/de/ironman/mccourse/entity/client/PenguinRenderer.java
+++ src/main/java/de/ironman/mccourse/entity/client/PenguinRenderer.java
@@ -1,21 +1,35 @@
 package de.ironman.mccourse.entity.client;
 
+import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.PoseStack;
 import de.ironman.mccourse.MCCourseMod;
 import de.ironman.mccourse.entity.custom.PenguinEntity;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.Map;
+
 public class PenguinRenderer extends MobRenderer<PenguinEntity, PenguinModel> {
+    private static final Map<PenguinVariant, ResourceLocation> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(PenguinVariant.class), map -> {
+                map.put(PenguinVariant.DEFAULT,
+                        ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID, "textures/entity/penguin/penguin.png"));
+                map.put(PenguinVariant.ALBINO,
+                        ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID, "textures/entity/penguin/penguin_albino.png"));
+                map.put(PenguinVariant.CLUB,
+                        ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID, "textures/entity/penguin/penguin_club.png"));
+            });
+
     public PenguinRenderer(EntityRendererProvider.Context context) {
         super(context, new PenguinModel(context.bakeLayer(ModModelLayers.PENGUIN)),0.75f);
     }
 
     @Override
     public ResourceLocation getTextureLocation(PenguinEntity penguinEntity) {
-        return ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID, "textures/entity/penguin/penguin.png");
+        return LOCATION_BY_VARIANT.get(penguinEntity.getVariant());
     }
 
     @Override
--- src/main/java/de/ironman/mccourse/entity/custom/PenguinEntity.java
+++ src/main/java/de/ironman/mccourse/entity/custom/PenguinEntity.java
@@ -1,11 +1,16 @@
 package de.ironman.mccourse.entity.custom;
 
 import de.ironman.mccourse.entity.ModEntities;
+import de.ironman.mccourse.entity.client.PenguinVariant;
 import de.ironman.mccourse.item.ModItems;
+import net.minecraft.Util;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -13,9 +18,13 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import org.jetbrains.annotations.Nullable;
 
 public class PenguinEntity extends Animal {
+    private static final EntityDataAccessor<Integer> VARIANT =
+            SynchedEntityData.defineId(PenguinEntity.class, EntityDataSerializers.INT);
+
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
 
@@ -72,5 +81,45 @@ public class PenguinEntity extends Animal {
         if(this.level().isClientSide()) {
             this.setupAnimationState();
         }
+    }
+
+    /* Variant */
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder builder) {
+        super.defineSynchedData(builder);
+        builder.define(VARIANT,0);
+    }
+
+    private int getTypeVariant() {
+        return this.entityData.get(VARIANT);
+    }
+
+    public PenguinVariant getVariant() {
+        return PenguinVariant.byId(this.getTypeVariant() & 255);
+    }
+
+    private void setVariant(PenguinVariant variant) {
+        this.entityData.set(VARIANT, variant.getId() & 255);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compound) {
+        super.addAdditionalSaveData(compound);
+        compound.putInt("Variant", this.getTypeVariant());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compound) {
+        super.readAdditionalSaveData(compound);
+        this.entityData.set(VARIANT, compound.getInt("Variant"));
+    }
+
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType spawnType,
+                                        @Nullable SpawnGroupData spawnGroupData) {
+        PenguinVariant variant = Util.getRandom(PenguinVariant.values(), this.random);
+        this.setVariant(variant);
+        return super.finalizeSpawn(level, difficulty, spawnType, spawnGroupData);
     }
 }
