f166e923580bf4a9f8b2f8218cacc32abf6c75b2
IronMan2297 <liamweigel1@icloud.com>
2025-08-05 22:28:39 +0200

    events

--- src/main/java/de/ironman/mccourse/event/ModEvents.java
+++ src/main/java/de/ironman/mccourse/event/ModEvents.java
@@ -1,13 +1,20 @@
 package de.ironman.mccourse.event;
 
 import de.ironman.mccourse.MCCourseMod;
+import de.ironman.mccourse.item.ModItems;
 import de.ironman.mccourse.item.custom.HammerItem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
 import java.util.HashSet;
@@ -37,6 +44,29 @@ public class ModEvents {
                 HARVESTED_BLOCKS.add(pos);
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void livingDamage(LivingDamageEvent event) {
+        if(event.getEntity() instanceof Sheep sheep) {
+            if(event.getSource().getDirectEntity() instanceof Player player) {
+                if(player.getMainHandItem().getItem() == ModItems.METAL_DETECTOR.get()) {
+                    player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a freaking Sheep with a Metal Detector!"));
+                }
+
+                if(player.getMainHandItem().getItem() == ModItems.TOMATO.get()) {
+                    player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a freaking Sheep with a tomato!"));
+                    sheep.addEffect(new MobEffectInstance(MobEffects.JUMP, 600, 50));
+                    player.getMainHandItem().shrink(1);
+                }
+
+                if(player.getMainHandItem().getItem() == Items.END_ROD) {
+                    player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a freaking Sheep with AN END ROD WHAT?!!"));
+                    sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 50));
+                    player.getMainHandItem().shrink(1);
+                }
             }
         }
     }
